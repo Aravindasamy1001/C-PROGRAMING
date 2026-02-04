ebe6cb6908cd85dc59500d7375413bab95309a17
Aravindasamy1001 <aravindasamy.v.1001@gmail.com>
2026-02-04 16:17:14 +0530

    Add files via upload

---
+++ llk.c
@@ -0,0 +1,14 @@
+ #include<stdio.h>
+int main(){
+float i=89.876;
+char ch='A';
+unsigned ux=789;
+int hex=255;
+long l=345678909; 
+printf("the float number:%f\n",i);
+printf("character :%c\n",ch);
+printf("unsigned value :%u\n",ux);
+printf("hexadecimal :%x\n",hex);
+printf("long int :%ld\n",l);
+return 0;
+}
