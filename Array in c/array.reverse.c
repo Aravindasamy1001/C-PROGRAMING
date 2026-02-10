fa984ed6bf6578f900d4ee1d662d2f7f98e27f45
Aravindasamy v <aravindasamy.v.1001@gmail.com>
2026-02-10 21:58:42 +0530

    Add files via upload

---
+++ Array in c/array.reverse.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+int ar[10];
+int i,n;
+ printf("enter a number of element:");
+scanf("%d",&n);
+
+printf("enter a elements:");
+for(i=0; i<=n; i++){
+scanf("%d",&ar[i]);
+}
+for(i=n-1; i>=0; i--){
+printf("array reverse=%d ",ar[i]);
+}
+return 0;
+}
