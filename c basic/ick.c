53bf0a0e200a3b3a46ead906b564ae358e9cc578
Aravindasamy v <aravindasamy.v.1001@gmail.com>
2026-02-13 13:18:07 +0530

    Add files via upload

---
+++ c basic/ick.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#define pi 3.14 //constant
+int main(){
+int radius=8; //keyword
+float place; //identifier
+place=pi*radius*radius;
+printf("place is %.3f\n",place);
+return 0;
+} 
+
