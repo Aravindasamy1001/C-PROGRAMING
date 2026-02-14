ebef2892ae209d09b8606d015c02dc9f2573fc2a
Aravindasamy v <aravindasamy.v.1001@gmail.com>
2026-02-14 22:00:16 +0530

    Add files via upload

---
+++ c basic/maxn.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+   int main(){
+
+    int marks[5]={89,65,76,88,45};//intiger array
+  
+   printf("student marks:\n");
+   for(int i=0;i<5;i++){
+
+   printf("student %d:%d\n",i+1,marks[i]);
+ }
+return 0;
+}
