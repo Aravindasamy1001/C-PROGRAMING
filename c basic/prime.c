eb88154119f266b4110289348c476c7c80ae5146
Aravindasamy v <aravindasamy.v.1001@gmail.com>
2026-02-14 22:06:31 +0530

    Add files via upload

---
+++ c basic/prime.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int g=15;
+    int i=2;
+     
+
+for  (i=2; i<=g/2; i++){
+
+if  (g%i==0){
+  
+printf("its not a prime number \n ");
+
+ break ;
+}
+}   
+ return 0;
+}
