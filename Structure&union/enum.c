2982483c4acbab75b2a282cfb1bd73283d60e08a
Aravindasamy v <aravindasamy.v.1001@gmail.com>
2026-02-15 23:32:58 +0530

    Add files via upload

---
+++ Structure&union/enum.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+enum days{
+monday,
+tuesday,
+wednesday,
+thursday,
+friday,
+saturday,
+sunday,
+};
+int main(){
+printf("monday=%d\n",monday);
+printf("tuesday=%d\n",tuesday);
+printf("wednesday=%d\n",wednesday);
+printf("thursday=%d\n",thursday);
+printf("friday=%d\n",friday);
+printf("saturday=%d\n",saturday);
+printf("sunday=%d\n",sunday);
+
+return 0;
+}
