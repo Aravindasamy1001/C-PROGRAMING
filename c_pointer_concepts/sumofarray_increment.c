51d9e851a66f5797494f0974215d8e080de4cd7d
Aravindasamy v <aravindasamy.v.1001@gmail.com>
2026-02-23 17:03:15 +0530

    Add files via upload

---
+++ c_pointer_concepts/sumofarray_increment.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+
+int as[5];
+int i;
+printf("enter five numbers:\n");
+for(i=0;i<5;i++){
+scanf("%d",&as[i]);
+}
+int *point=&as[0];
+int sum=0;
+for(i=0;i<5;i++){
+sum=*point+as[i];
+point++;
+}
+printf("sum of array:%d\n",sum);
+return 0;
+}
