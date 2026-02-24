19c66ae86a23f28eda64dd6e948920f3f2399ded
Aravindasamy v <aravindasamy.v.1001@gmail.com>
2026-02-24 18:17:36 +0530

    Add files via upload

---
+++ c_pointer_concepts/array_matrix2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+
+int a[3][3]={{1,2,3},{4,5,6},{7,8,9},};
+int (*p)[3]=a;
+
+printf("2D matrix array:\n");
+
+for(int i=0;i<3;i++){
+  for(int j=0;j<3;j++){
+
+printf("%d",(*p)[j]);
+}
+printf("\n");
+p++;
+}
+
+return 0;
+}
+
