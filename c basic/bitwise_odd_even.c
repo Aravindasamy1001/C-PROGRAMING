4e4682903b0bd82e7c728b09772679e15351dfc4
Aravindasamy v <aravindasamy.v.1001@gmail.com>
2026-02-27 23:31:40 +0530

    Add files via upload

---
+++ c basic/bitwise_odd_even.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+
+int a;
+printf("enter a intiger number:\n");
+scanf("%d",&a);
+
+if(a&1)
+{
+printf("its a odd number");
+}
+else
+{
+printf("its a even number");
+}
+
+return 0;
+}
