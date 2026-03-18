254939f6ec17ae48bc553cd220170d092859f2c3
Aravindasamy v <aravindasamy.v.1001@gmail.com>
2026-03-18 17:28:41 +0530

    Add files via upload

---
+++ consistent_practice.c/bitwise.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+int a=5;
+int b=7;
+int i;
+
+printf("bitwise And:%d\n",i=a&b);
+printf("bitwise OR:%d\n",i=a|b);
+printf("bitwise xor:%d\n",i=a^b);
+printf("bitwise not:%d\n",i=~a);
+printf("bitwise left shift:%d\n",i=a<<1);
+printf("bitwise right shift:%d\n",i=a>>2);
+
+return 0;
+}
