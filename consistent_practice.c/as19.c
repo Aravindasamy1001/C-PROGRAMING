889b49acef4ba0e56abfedfb81b11a2c0596fb8c
Aravindasamy v <aravindasamy.v.1001@gmail.com>
2026-03-29 22:06:30 +0530

    Add files via upload

---
+++ consistent_practice.c/as19.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+
+int row;
+int space;
+int star;
+int n=5;
+
+for(row=1;row<=n;row++){
+
+   for(space=0;space<row-1;space++){
+
+      printf(" ");
+   }
+   for(star=0;star<2*row-1;star++){
+     printf("*");
+   }
+printf("\n");
+}
+
+return 0;
+
+}
