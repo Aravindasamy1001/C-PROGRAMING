cb5ead1761effe35172bf48ea72f685bd8d8799f
Aravindasamy v <aravindasamy.v.1001@gmail.com>
2026-04-05 22:02:04 +0530

    Add files via upload

---
+++ Makefile/pattern02/pattern.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include "pattern.h"
+
+void patterns(int row,int n)
+{
+
+int space;
+int star;
+
+for(row=1;row<=5;row++){
+
+    for(space=1;space<row;space++){
+
+        printf(" ");
+     }
+    for(star=0;star<=n-row;star++){
+
+       printf("*");
+     }
+
+printf("\n");
+}
+
+}
+
