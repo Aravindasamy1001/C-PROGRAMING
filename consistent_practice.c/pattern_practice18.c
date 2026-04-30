a230b02f2ca9548075514d274e76109474827164
Aravindasamy v <aravindasamy.v.1001@gmail.com>
2026-04-30 16:07:08 +0530

    Add files via upload

---
+++ consistent_practice.c/pattern_practice18.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+
+int x=5;
+
+for(int i=0;i<x;i++){
+
+   for(int j=0;j<i;j++){
+      printf(" ");
+      }
+
+   for(int d=0;d<x;d++){
+      printf("*");
+      }
+
+printf("\n");
+}
+
+return 0;
+}
