ad869f45024f8071bcbc4438fa035c55ea09c419
Aravindasamy v <aravindasamy.v.1001@gmail.com>
2026-05-05 22:41:17 +0530

    Add files via upload

---
+++ Makefile/Pointer/main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include "pointer.h"
+
+int main(){
+
+	char word[20]={"tremendous"};
+
+	printf("count one char in tremendous word\n");
+
+
+int a=countchar(word);
+
+printf("char E=%d\n",a);
+
+return 0;
+}
+
