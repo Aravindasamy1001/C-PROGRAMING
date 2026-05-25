65386e8f540acb1c093c20a4e40554f2669904ab
Aravindasamy v <aravindasamy.v.1001@gmail.com>
2026-05-25 21:44:47 +0530

    Update debug31.c

--- DEBUG_PRACTICE/debug31.c
+++ DEBUG_PRACTICE/debug31.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+int main(){
 
+ void pa(int *a,int n);//prototype.
+
+ int arr[5]={5,4,3,2,1};
+
+ pa(arr,5);
+ return 0;
+}
+
+ void pa(int *a,int n){ 
+int i;
+for(i=0;i<n;i++){
+	 printf("%d",*(a++)+i);
+ }
+ }
