2aee8189ae2081977d43c09d3cd8219f92c728e9
Aravindasamy v <aravindasamy.v.1001@gmail.com>
2026-05-25 19:42:20 +0530

    Update debug30.c

--- DEBUG_PRACTICE/debug30.c
+++ DEBUG_PRACTICE/debug30.c
@@ -1,3 +1,16 @@
+#include<stdio.h>
 
+int main(){
 
+	int var1,var2,var3,minmax;
+
+	var1=6;
+	var2=5;
+	var3=4;
+
+	minmax=(var1>var2)?(var1>var3)?var1 : var3:(var2>var3)? var2:var3;
+
+	printf("ternary:%d\n",minmax);
+	return 0;
+}
 
