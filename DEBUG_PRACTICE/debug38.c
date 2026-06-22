1483054d70f45df6ab995efc2c5037a76dd3bdcc
Aravindasamy v <aravindasamy.v.1001@gmail.com>
2026-06-22 22:27:31 +0530

    Update debug38.c

--- DEBUG_PRACTICE/debug38.c
+++ DEBUG_PRACTICE/debug38.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main() {
+    struct s{
+        char *p;
+    }
+    static struct s a[]={"RAVI"};
+    struct s *pp=a;
+    printf("\n%s",++pp->p);
+    
+    return 0;
+}
