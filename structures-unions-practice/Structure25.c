0f9c9f96d4c1995f689492a4359180037cc9cdfd
Aravindasamy v <aravindasamy.v.1001@gmail.com>
2026-07-09 15:52:46 +0530

    Update Structure25.c

--- structures-unions-practice/Structure25.c
+++ structures-unions-practice/Structure25.c
@@ -1,1 +1,26 @@
+#include<stdio.h>
 
+struct fam{
+
+int count;
+int data[];
+};
+
+struct df{
+
+float f;
+double d;
+float ff;
+
+};
+
+int main(){
+struct fam fam1;
+struct df df1;
+
+printf("%zu\n",sizeof(fam1));
+printf("%zu\n",sizeof(df1));
+
+
+return 0;
+}
